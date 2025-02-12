56486a5e84cc69841132044f985c7f9d5419bb56
RajuCodz <itsraajjjuuuu@gmail.com>
2025-02-12 22:17:17 +0530

    Optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     domains: ["i.pinimg.com", "image.tmdb.org"],
   },
 };
+const withVideos = require("next-videos");
 
+module.exports = withVideos();
 export default nextConfig;
